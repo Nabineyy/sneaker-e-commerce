46b6a47942d038b945c42e6f2fcfb981afe1c757
Nabineyy <adhikarinabin301@gmail.com>
2025-02-04 16:01:51 +0545

    updated

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var MainImg = document.getElementById("MainImg");
 var smallimg = document.getElementsByClassName("small-img");
-
         smallimg[0].onclick =function(){
             MainImg.src = smallimg[0].src;
         }
@@ -13,6 +12,7 @@ var smallimg = document.getElementsByClassName("small-img");
         smallimg[3].onclick =function(){
             MainImg.src = smallimg[3].src;
         }
+
 
 
 
